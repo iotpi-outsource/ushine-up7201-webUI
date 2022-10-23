1e39c42076e849d002f377f18183ab582d795955
Yang Hongbo <yanghongbo@iotpi.io>
2022-10-23 15:54:28 +0800

    add device

--- app/lib/components/fsk_devices.jsx
+++ app/lib/components/fsk_devices.jsx
@@ -54,8 +54,13 @@ export default class networkComponent extends React.Component {
       errorMsg: null,
     };
 
+    this.state.input = {
+      addr: null,
+      no: null,
+      freq: null,
+    };
     this.state.devices = [];
-    // this._handleSettingDevices = ::this._handleSettingDevices;
+    this._handleAddDevice = ::this._handleAddDevice;
   }
 
   componentWillMount() {
@@ -97,6 +102,17 @@ export default class networkComponent extends React.Component {
     });
   }
 
+  _handleAddDevice() {
+    let device = {
+      addr: this.state.input.addr,
+      no: this.state.input.no,
+      freq: this.state.input.freq,
+    };
+    this.setState((state) => ({
+      devices: [...state.devices, device],
+    }));
+  }
+
   componentDidMount() {
     return true;
   }
@@ -111,31 +127,80 @@ export default class networkComponent extends React.Component {
     return (
       <div>
         <Card>
-        <div style={ styles.content }>
+          <div style={ styles.content }>
           <h3>{__("Devices")}</h3>
-          <div style={{
-                 display: 'flex',
-                 flexDirection: 'row',
-                 justifyContent: 'space-between',
-               }}>
-            <RaisedButton
-              linkButton
-              label={__('Cancel')}
-              style={{
-                width: '236px',
-                flexGrow: 1,
-                textAlign: 'center',
-                marginTop: '20px',
-                marginBottom: '20px',
-                marginRight: '10px',
-              }}
-              backgroundColor="#EDEDED"
-              labelColor="#999A94" />
+            <TextField
+              type="text"
+              value={ this.state.input.addr }
+              style={{ width: '100%' }}
+              onChange={
+                (e) => {
+                  this.setState({
+                    input: {
+                      addr: e.target.value,
+                      no: this.state.input.no,
+                      freq: this.state.input.freq,
+                    }
+                  });
+                }
+              }
+              underlineFocusStyle={{ borderColor: Colors.amber700 }}
+              floatingLabelStyle={{ color: 'rgba(0, 0, 0, 0.498039)' }}
+              floatingLabelText={
+                <div>
+                  { __("Device Address") } <b style={{ color: 'red' }}>*</b>
+                </div>
+              } />
+            <TextField
+              type="text"
+              value={ this.state.input.no }
+              style={{ width: '100%' }}
+              onChange={
+                (e) => {
+                  this.setState({
+                    input: {
+                      addr: this.state.input.addr,
+                      no: e.target.value,
+                      freq: this.state.input.freq,
+                    }
+                  });
+                }
+              }
+              underlineFocusStyle={{ borderColor: Colors.amber700 }}
+              floatingLabelStyle={{ color: 'rgba(0, 0, 0, 0.498039)' }}
+              floatingLabelText={
+                <div>
+                  { __("Device No.") } <b style={{ color: 'red' }}>*</b>
+                </div>
+              } />
+            <TextField
+              type="text"
+              value={ this.state.input.freq }
+              style={{ width: '100%' }}
+              onChange={
+                (e) => {
+                  this.setState({
+                    input: {
+                      addr: this.state.input.addr,
+                      no: this.state.input.no,
+                      freq: e.target.value,
+                    }
+                  });
+                }
+              }
+              underlineFocusStyle={{ borderColor: Colors.amber700 }}
+              floatingLabelStyle={{ color: 'rgba(0, 0, 0, 0.498039)' }}
+              floatingLabelText={
+                <div>
+                  { __("Device Freq.") } <b style={{ color: 'red' }}>*</b>
+                </div>
+              } />
             <RaisedButton
               linkButton
               secondary
-              label={__('Configure & Restart')}
+              label={__('Add')}
               backgroundColor={ Colors.amber700 }
+              onTouchTap={ this._handleAddDevice }
               style={{
                 width: '236px',
                 flexGrow: 1,
@@ -144,6 +209,11 @@ export default class networkComponent extends React.Component {
                 marginBottom: '20px',
                 marginLeft: '10px',
               }} />
+          <div style={{
+                 display: 'flex',
+                 flexDirection: 'row',
+                 justifyContent: 'space-between',
+               }}>
           </div>
           <List>
             {
