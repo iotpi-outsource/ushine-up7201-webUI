e0f243bbfe318b7718ccdd81c8eaa8c659d91fce
Yang Hongbo <yanghongbo@iotpi.io>
2022-11-06 13:26:04 +0800

    input user account instead of default root

--- app/lib/components/sysinfo.jsx
+++ app/lib/components/sysinfo.jsx
@@ -325,11 +325,33 @@ export default class sysinfoComponent extends React.Component {
 
         <h3 style={ [styles.h3Top, { marginTop: '-15px' }] }>{ __('Account information') }</h3>
 
-        <h3 style={ styles.panelTitle }>{ __('Account') }</h3>
-        <p style={ styles.panelContent }>root(default)</p>
+        {/*<h3 style={ styles.panelTitle }>{ __('Account') }</h3>
+           <p style={ styles.panelContent }>root(default)</p>*/}
+        <TextField
+          hintText={ __('Account') }
+          style={{ width: '100%', marginTop: '-15px' }}
+          underlineStyle={{ borderColor: '#D1D2D3' }}
+          defaultValue={ this.state.user }
+          floatingLabelStyle={{ color: 'rgba(0, 0, 0, 0.498039)' }}
+          underlineFocusStyle={{ borderColor: Colors.amber700 }}
+          type="text"
+          errorStyle={{ borderColor: Colors.amber700 }}
+          errorText={ errorText }
+          onChange={
+            (e) => {
+              this.setState({
+                user: e.target.value,
+              });
+            }
+          }
+          floatingLabelText={
+            <div>
+              { __('Account') } <b style={{ color: 'red' }}>*</b>
+            </div>
+          } />
         <TextField
           hintText={ __('Password') }
-          style={{ width: '100%', marginTop: '-40px' }}
+          style={{ width: '100%', marginTop: '0px' }}
           underlineStyle={{ borderColor: '#D1D2D3' }}
           defaultValue={ this.state.password }
           floatingLabelStyle={{ color: 'rgba(0, 0, 0, 0.498039)' }}
